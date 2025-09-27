1500a848d534bde0d84eba48c51a594826c44857
JPelenberg <annejess5@gmail.com>
2025-09-27 16:21:35 -0400

    Create index.js file for the Home component

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to Studio40!</h1>
+      <p>
+        <a href="/invitation">Go to Invitation</a> | <a href="/admin">Go to Admin</a>
+      </p>
+    </main>
+  );
+}
